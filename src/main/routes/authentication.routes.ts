0fd59029c30dc5e3e82ae77b1765a5be3f09f1cf
JPFR01 <joao-pedrofonseca22@hotmail.com>
2026-02-20 23:37:40 -0300

    feat: validação de segurança meta

--- src/main/routes/authentication.routes.ts
+++ src/main/routes/authentication.routes.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import {Router, Request, Response} from 'express';
 import {adaptRoute} from '@/main/adapter/express/expressRouteAdapter';
 import {LoginControllerFactory} from '../factories/controllers/authentication/login/LoginControllerFactory';
 import {RefreshTokenControllerFactory} from '../factories/controllers/authentication/refresh-token/RefreshTokenControllerFactory';
@@ -6,4 +6,15 @@ import {RefreshTokenControllerFactory} from '../factories/controllers/authentica
 export default (router: Router): void => {
     router.post('/v1/login', adaptRoute(LoginControllerFactory()));
     router.post('/v1/refreshToken', adaptRoute(RefreshTokenControllerFactory()));
+
+    router.post('/v1/webhook/meta/verify', (req: Request, res: Response) => {
+        const hubVerifyToken = req.body['hub.verify_token'];
+        const hubVerifyTokenMeta = process.env.HUB_VERIFY_TOKEN_META;
+
+        if (hubVerifyToken === hubVerifyTokenMeta) {
+            return res.status(204).send();
+        }
+
+        return res.status(400).json({ error: 'Parâmetros inválidos' });
+    });
 };
